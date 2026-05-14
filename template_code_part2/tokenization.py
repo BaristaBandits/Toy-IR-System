4e849ebf725944079c2bac21942bafd8f861e55b
Swathi Narashiman <143377975+BaristaBandits@users.noreply.github.com>
2026-05-14 23:35:02 +0530

    Update tokenization.py

--- template_code_part2/tokenization.py
+++ template_code_part2/tokenization.py
@@ -9,19 +9,7 @@ import spacy
 class Tokenization():
 
 	def naive(self, text):
-		"""
-		Tokenization using a Naive Approach
 
-		Parameters
-		----------
-		arg1 : list
-			A list of strings where each string is a single sentence
-
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-		"""
 
 		#tokenizedText = None
 
@@ -37,19 +25,7 @@ class Tokenization():
 
 
 	def pennTreeBank(self, text):
-		"""
-		Tokenization using the Penn Tree Bank Tokenizer
-
-		Parameters
-		----------
-		arg1 : list
-			A list of strings where each string is a single sentence
-
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-		"""
+		
 
 		#tokenizedText = None
 
@@ -65,19 +41,7 @@ class Tokenization():
 
 
 	def spacyTokenizer(self, text):
-		"""
-		Tokenization using spaCy
-
-		Parameters
-		----------
-		arg1 : list
-			A list of strings where each string is a single sentence
-
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of tokens
-		"""
+		
 
 		#tokenizedText = None
 
